7b11c144d1c77cccd3042a672b589cd0050f7b1e
David Sampedro <david.sampi1990@gmail.com>
2026-02-22 21:08:16 -0500

    Configuración de DB para producción y scripts de inicio

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const { Pool } = require("pg");
+/*const { Pool } = require("pg");
 require('dotenv').config(); // 👈 ¡ESTA LÍNEA ES CLAVE! Carga tus variables del .env
 
 const pool = new Pool({
@@ -14,4 +14,30 @@ pool.on('connect', () => {
   console.log('🐘 Conectado a la base de datos PostgreSQL');
 });
 
-module.exports = pool;
+module.exports = pool;*/
+
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// Usamos la URL de la base de datos de la variable de entorno
+// Si no existe (en local), podrías poner una por defecto, 
+// pero en Render configuraremos esta variable.
+const connectionString = process.env.DATABASE_URL;
+
+const db = new Pool({
+  connectionString: connectionString,
+  ssl: {
+    // Esto es obligatorio para conectar con Render desde afuera
+    rejectUnauthorized: false,
+  },
+});
+
+db.on("connect", () => {
+  console.log("✅ Conectado a la base de Datos en la Nube");
+});
+
+db.on("error", (err) => {
+  console.error("❌ Error inesperado en el pool de Postgres", err);
+});
+
+module.exports = db;
