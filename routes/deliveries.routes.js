1a229e52de2dffce30ae6a2fb3381150d736eb70
David Sampedro <david.sampi1990@gmail.com>
2026-02-22 21:58:42 -0500

    Eliminar proteccion de rutas en deliveries

--- routes/deliveries.routes.js
+++ routes/deliveries.routes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const router = require("express").Router();
 const controller = require("../controllers/deliveries.controller");
 const auth = require("../middleware/auth.middleware");
@@ -13,5 +13,23 @@ router.post("/", controller.createDelivery);
 router.get("/total", auth, controller.totalGlobal);
 router.delete("/:id", controller.deleteDelivery);
 
+
+module.exports = router;*/
+
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/deliveries.controller");
+
+// Eliminamos "auth" de las rutas para permitir el acceso desde el Frontend publicado
+router.get("/progress", controller.progress);
+router.get("/groups", controller.byGroup);
+router.get("/total", controller.totalGlobal);
+
+// Rutas base para obtener y crear entregas
+router.get("/", controller.getDeliveries);
+router.post("/", controller.createDelivery);
+
+// Ruta para eliminar
+router.delete("/:id", controller.deleteDelivery);
 
 module.exports = router;
