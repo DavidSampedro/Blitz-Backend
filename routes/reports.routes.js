ce09190f3e88ef1c7ea13aef4dfe1751bd185f76
David Sampedro <david.sampi1990@gmail.com>
2026-02-24 23:38:55 -0500

    inicio de reportes4

--- routes/reports.routes.js
+++ routes/reports.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const reportsController = require("../controllers/reports.controller");
-
+const reportRoutes = require('./routes/report.routes');
+// ...
+app.use('/api/reports', reportRoutes); // Esto crea la ruta /api/reports/global
 router.get("/global", reportsController.getGlobalReport);
 
 module.exports = router;
